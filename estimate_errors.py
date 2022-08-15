85eb6773e47578c785b4ff38c61ef84a67e84236
dehui333 <dehui333@gmail.com>
2022-08-15 13:17:42 +0800

    fix path issue in estimate_errors

--- estimate_errors.py
+++ estimate_errors.py
@@ -45,7 +45,10 @@ def estimate_error_bam(bam_path):
 Given a path to a read set (fasta/q) and a reference assembly, estimate error rate in read set.
 '''
 def estimate_error_reads(reads_path, reference_path, keep_sam, num_threads=1): 
-    sam_path = os.path.dirname(reads_path) + '/'+ Path(reads_path).stem + '_to_' + Path(reference_path).stem + '.sam'
+    reads_dir = os.path.dirname(reads_path)
+    if reads_dir != '':
+        reads_dir += '/' 
+    sam_path =  reads_dir + Path(reads_path).stem + '_to_' + Path(reference_path).stem + '.sam'
     already_has_sam = os.path.exists(sam_path)
     if not already_has_sam:
         with open(sam_path, 'w') as sam_file:
@@ -63,7 +66,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Estimate the error rate in a set of reads.')
     parser.add_argument('-i', '--input', type=str, help='path of the input fasta/q file.')
     parser.add_argument('-r', '--ref', type=str, help='path of the reference assembly.')
-    parser.add_argument('-t', '--num_threads', type=int, default=1, help='number of threads for mapping.')
+    parser.add_argument('-t', '--num_threads', type=int, default=1, help='number of threads for mapping. [1]')
     parser.add_argument('-k', action='store_true', help='whether to keep sam.')
     args = parser.parse_args()
     subs_rate, ins_rate, del_rate = estimate_error_reads(args.input, args.ref, args.k, args.num_threads)
